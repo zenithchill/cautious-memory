2be430174195d417e99ffeb538757d86f0f78560
Sudhanshu sharma <249450550+zenithchill@users.noreply.github.com>
2026-05-22 21:01:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+    lines: [
+        "Dear Shagun",
+        "Every moment with you feels unreal",
+        "You turned ordinary days into memories",
+        "Your smile became my favorite place",
+        "No matter where life goes",
+        "I just want you beside me",
+        "I love you forever ❤️"
+    ],
+
+    // IMPORTANT FORMAT:
+    // YYYY-MM-DD HH:MM:SS
+    // Change this to your real date
+    date: "2025-01-01",
+
+    names: ["Jitesh", "Shagun"]
+}
